5b983b08855b91115eebf9a4ee30df8e216badfc
Gers2017 <Gers2017@outlook.com>
2022-06-10 19:26:48 -0600

    Io
    - update keymaps: lua/core/keymaps.lua
    - update options and guicursor: lua/core/options.lua
    - update indent_blankline setup: lua/plugins/config/indent-blankline.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,53 +11,64 @@ local M = {}
 
 M.setup = function ()
   -- User
+  map('n', '<C-z>', 'u')
   map('n', 'Y', 'Vy')
-  
+  map('n', '<C-x>', 'dd')
+  map('n', '0', '^')
+  map('n', '<home>', '^')
+  map('n', '<end>', '$')
+  map('n', '<C-Right>', 'w')
+  map('n', '<C-Left>', 'b')
+ 
   -- Telescope
   map('n', '<C-p>', ':Telescope find_files<cr>')
-  map('n', '<ldeader>tg', ':Telescope live_grep<cr>')
-  
+  map('n', '<leader>tg', ':Telescope live_grep<cr>')
+ 
   -- NvimTree
   map('n', '<C-b>', ':NvimTreeToggle<cr>')
   map('n', '<leader>nc', ':NvimTreeClose<cr>')
   map('n', '<leader>nf', ':NvimTreeFocus<cr>')
   map('n', '<C-f>', ':NvimTreeFocus<cr>')
-  
+ 
   -- Window navigation
-  map('n', '<C-h>', '<C-w>h')
-  map('n', '<C-j>', '<C-w>j')
-  map('n', '<C-k>', '<C-w>k')
-  map('n', '<C-l>', '<C-w>l')
-  
+  map('n', '<leader>wh', '<C-w>h')
+  map('n', '<leader>wj', '<C-w>j')
+  map('n', '<leader>wk', '<C-w>k')
+  map('n', '<leader>wl', '<C-w>l')
+  map('n', '<leader>ww', '<C-w>w')
+ 
   -- Buffers
   map('n', '<A-Right>', '<cmd>bn<cr>')
   map('n', '<A-Left>', '<cmd>bp<cr>')
   map('n', '<A-c>', '<cmd>bd<cr>')
-  
+ 
   -- Resize with arrows
-  map('n', '<C-Up>', ':resize -2<cr>')
-  map('n', '<C-Down>', ':resize +2<cr>')
-  map('n', '<C-Left>', ':vertical resize -2<cr>')
-  map('n', '<C-Right>', ':vertical resize +2<cr>')
-  
+  map('n', '<S-Up>', ':resize -2<cr>')
+  map('n', '<S-Down>', ':resize +2<cr>')
+  map('n', '<S-Left>', ':vertical resize -2<cr>')
+  map('n', '<S-Right>', ':vertical resize +2<cr>')
+ 
   -- Move text up and down normal mode
-  map('n', '<A-j>', '<Esc>:m .+1<cr>==gi')
-  map('n', '<A-k>', '<Esc>:m .-2<cr>==gi')
-  
+  map('n', '<A-Up>', '<Esc>:m .-2<cr>==gn')
+  map('n', '<A-Down>', '<Esc>:m .+1<cr>==gn')
+
+
+ -- Move text up and down edit mode (I)
+  map('i', '<A-Up>', '<Esc>:m .-2<cr>==gi')
+  map('i', '<A-Down>', '<Esc>:m .+1<cr>==gi')
   -- Stay in indent mode
   map('v', '<', '<gv')
   map('v', '>', '>gv')
-  
+ 
   -- Move text up and down visual mode
-  map('v', '<A-j>', ':m .+1<cr>==')
-  map('v', '<A-k>', ':m .-2<cr>==')
+  map('v', '<A-Up>', ':m .-2<cr>==gv')
+  map('v', '<A-Down>', ':m .+1<cr>==gv')
   map('v', 'p', '"_dP')
-  
+ 
   -- Move text up and down visual block mode
-  map("x", "J", ":move '>+1<cr>gv-gv")
-  map("x", "K", ":move '<-2<cr>gv-gv")
-  map("x", "<A-j>", ":move '>+1<cr>gv-gv")
-  map("x", "<A-k>", ":move '<-2<cr>gv-gv")
+  map("x", "<A-Up>", ":move '<-2<cr>gv-gv")
+  map("x", "<A-Down>", ":move '>+1<cr>gv-gv")
+
 end
 
 M.lspconfig = function ()
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,29 +8,48 @@ o.hidden = true
 o.clipboard = 'unnamedplus'
 o.scrolloff=10
 o.sidescrolloff = 8
-o.tabstop = 2
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
 o.expandtab = true
-o.shiftwidth = 2
-o.softtabstop = 2
+o.smartindent = true
 o.smarttab = true
 o.autoindent = true
+o.cindent = true
 o.joinspaces = false
+o.incsearch = true
+o.signcolumn = 'yes'
 o.completeopt = { "menuone", "noselect" }
 o.splitright = true
 o.splitbelow = true
-o.termguicolors = true
 o.inccommand='split'
 o.background = 'dark'
+o.termguicolors = true
+
+-- indent-blankline
+o.list = true
+o.listchars:append("space:⋅")
 
 vim.g.tokyonight_style = "night"
 vim.cmd [[ colorscheme tokyonight ]]
 
 vim.cmd [[
   syntax enable
+  set guicursor=
+  set nohlsearch
+  set noerrorbells
   set shortmess+=c
   filetype plugin on
   set updatetime=300
   set whichwrap+=<,>,[,],h,l
 ]]
+
+-- indent-blankline config
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
 
